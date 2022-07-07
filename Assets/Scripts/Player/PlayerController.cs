ddc7cce20b731083ce9e3999d8aa9acf38f53825
CesarJZO <cesarjzo@outlook.com>
2022-07-07 16:58:09 -0500

    Add pixel perfect camera and adjust player input

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,14 +8,18 @@ namespace Player
     {
         public static PlayerController Instance { get; private set; }
 
-        [Header("Settings")] 
-        [SerializeField] private float speed;
+        [Header("Settings")]
         
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpForce;
+
         [Header("Input")]
+        
         [SerializeField] private float smoothTime;
 
         private Vector2 _rawInput;
         private Vector2 _smoothInput;
+        [SerializeField] private float deadZone;
         public float XInput { get; private set; }
         public float YInput { get; private set; }
 
@@ -24,26 +28,23 @@ namespace Player
         private Animator _animator;
         private readonly int _moveID = Animator.StringToHash("XInput");
         private readonly int _jumpID = Animator.StringToHash("Jump");
-        
+
         #endregion
 
         #region Physics
 
-        [Header("Physics")]
-        [SerializeField] private float groundDistance;
-
+        [Header("Physics")] [SerializeField] private float groundDistance;
         [SerializeField] private LayerMask groundMask;
-        public bool Grounded => Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundMask);
-
+        public RaycastHit2D Grounded => Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundMask);
         #endregion
-        
+
         #region Components
 
         private Rigidbody2D _rigidbody;
         private PlayerInput _playerInput;
         private InputAction _moveAction;
         private InputAction _jumpAction;
-        
+
         #endregion
 
         #region MonoBehaviour Methods
@@ -60,21 +61,29 @@ namespace Player
 
         private void Update()
         {
-            var velocity = Vector2.zero;
+            var currentVelocity = Vector2.zero;
+
 
             _rawInput = _moveAction.ReadValue<Vector2>();
-            _smoothInput = Vector2.SmoothDamp(_smoothInput, _rawInput, ref velocity, smoothTime);
+            _smoothInput = Vector2.SmoothDamp(_smoothInput, _rawInput, ref currentVelocity, smoothTime);
+            
+            if (_smoothInput.magnitude <= deadZone)
+                _smoothInput = Vector2.zero;
+
             XInput = _smoothInput.x;
             YInput = _smoothInput.y;
-            if (_jumpAction.WasPressedThisFrame())
+            if (_jumpAction.WasPressedThisFrame() && Grounded)
+            {
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 _animator.SetTrigger(_jumpID);
+            }
         }
 
         private void FixedUpdate()
         {
             _rigidbody.velocity = new Vector2(XInput * speed, _rigidbody.velocity.y);
         }
-        
+
         private void LateUpdate()
         {
             _animator.SetFloat(_moveID, XInput);
